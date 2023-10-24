2bc8177acf350324df063bb204e39d52a38605bc
nine-9-days <ec2-user@ip-172-31-21-44.ap-northeast-1.compute.internal>
2023-10-24 15:40:35 +0900

    seeder commit

---
+++ database/seeders/PartAdd_Seeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PartAdd_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('parts')->insert([
+            'name'=>'病斑部位',
+        ]);
+    }
+}
---
+++ database/seeders/featuresAdd_Seeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class featuresAdd_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('features')->insert([
+            'name'=>'色',
+            'feature'=>'黒色である',
+        ]);
+        
+    }
+}
